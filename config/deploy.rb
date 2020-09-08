71e1e9105b9827ea1fe0bd02c50a50c80204f0f3
Andy Braga <bragovo@gmail.com>
2020-09-08 18:30:43 +0300

    Clear manifest backup

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,3 +12,5 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 set :keep_releases, 3
 
 set :ssh_options, forward_agent: true
+
+Rake::Task['deploy:assets:backup_manifest'].clear_actions

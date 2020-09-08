ff77214b3700426c39d2c9dffab39093b6fc757c
Andy Braga <ab@luckypike.com>
2020-09-08 17:33:24 +0300

    Use pg position func

--- app/controllers/api/v1/procedures_controller.rb
+++ app/controllers/api/v1/procedures_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ProceduresController < ApplicationController
   def index
     @procedures = Procedure.all
+    if params[:q].present?
+      @procedures = @procedures
+        .where("POSITION(LOWER('#{params[:q]}') IN LOWER(title)) > 0")
+        .order("POSITION(LOWER('#{params[:q]}') IN LOWER(title))")
+    end
   end
 end
--- app/javascript/components/Procedures.js
+++ app/javascript/components/Procedures.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 export default function Procedures () {
+  const [procedures, setProcedures] = useState()
+  const [input, setInput] = useState('')
+
+  const _fetch = async () => {
+    const { data } = await axios.get('http://localhost:3000/api/v1/procedures.json', { params: { q: input }})
+
+    setProcedures(data.procedures)
+  }
+
+  useEffect(() => {
+    _fetch()
+  }, [input])
+
   return (
-    <div>Procedures</div>
+    <div>
+      <h1>
+        Procedures
+      </h1>
+
+      <div>
+        <input
+          value={input}
+          onChange={({ target: { value } }) => setInput(value)}
+        />
+      </div>
+
+      {procedures &&
+        <div>
+          {procedures.map(procedure =>
+            <div key={procedure.id}>
+              {procedure.title}
+            </div>
+          )}
+        </div>
+      }
+    </div>
   )
 }
